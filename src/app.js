65ef8a0f4c2ebba1afc386717742b8becb177fc7
oblivion-deeep <oldmanjenkins09@outlook.com>
2025-06-03 15:36:12 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compression());
 
 
-app.use('/api/movies', movieRoutes);
+app.use('/api/v1', movieRoutes);
 
 app.get('/', (req, res) => {
   res.json({
